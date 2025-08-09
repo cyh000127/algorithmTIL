6d26c3d9673688e6dd296bc931c8d24118a5b976
최윤혁 <okccc5@naver.com>
2025-08-09 11:20:54 +0900

    [D2] Title: [IL lv2 기본기 다지기] 문제 3: 계곡 찾기, Time: 73 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D2/24100. ［IL lv2 기본기 다지기］ 문제 3： 계곡 찾기/［IL lv2 기본기 다지기］ 문제 3： 계곡 찾기.java
+++ SWEA/D2/24100. ［IL lv2 기본기 다지기］ 문제 3： 계곡 찾기/［IL lv2 기본기 다지기］ 문제 3： 계곡 찾기.java
@@ -6,19 +6,14 @@ import java.util.StringTokenizer;
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int T = Integer.parseInt(br.readLine());
 		// 테스트 케이스
+		int T = Integer.parseInt(br.readLine());
 		for (int test = 1; test <= T; test++) {
-			// 배열 크기 N
+			// 지도 크기
 			int N = Integer.parseInt(br.readLine());
+
+			// 배열 선언 및 산 배치
 			int[][] arr = new int[N][N];
-
-			// 방향도 미리 선언
-			int[] dr = { 1, 0, -1, 0 };
-			int[] dc = { 0, 1, 0, -1 };
-
-			// 배열에 값 할당
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
@@ -26,28 +21,29 @@ public class Solution {
 				}
 			}
 
-			// 구석 제외 범위 1 ~ N-1
-			// 계곡 찾기
-			int cnt = 0;
+			int[] dr = { -1, 0, 1, 0 };
+			int[] dc = { 0, 1, 0, -1 };
 
+			int cnt = 0;
 			for (int i = 1; i < N - 1; i++) {
 				for (int j = 1; j < N - 1; j++) {
 					boolean isTrue = true;
-
-					for (int d = 0; d < 4; d++) {
-						int nr = i + dr[d];
-						int nc = j + dc[d];
+					
+					for (int c = 0; c < 4; c++) {
+						int nr = i + dr[c];
+						int nc = j + dc[c];
 
 						if (arr[i][j] >= arr[nr][nc]) {
 							isTrue = false;
 							break;
 						}
 					}
-					if (isTrue)
-						cnt++;
+					if(isTrue) cnt++;
 				}
+
 			}
-			System.out.println("#"+test + " "+cnt);
+			System.out.println("#"+test+" "+cnt);
+
 		}
 	}
 }
