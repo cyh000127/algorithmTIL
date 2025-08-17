b330da8b3a7c5205c1d5fe7055db1bebc3519057
최윤혁 <okccc5@naver.com>
2025-08-17 10:57:58 +0900

    [D3] Title: 퍼펙트 셔플, Time: 110 ms, Memory: 31,616 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
@@ -1,57 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
 
-		for (int tc = 1; tc <= T; tc++) {
+		// 테스트 케이스 지정
+		int T = Integer.parseInt(br.readLine());
+		for (int test = 1; test <= T; test++) {
+			// 카드 개수
 			int card = Integer.parseInt(br.readLine());
+
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			Queue<String> queue1 = new LinkedList<String>();
-			Queue<String> queue2 = new LinkedList<String>();
+			LinkedList<String> arr1 = new LinkedList<String>();
+			LinkedList<String> arr2 = new LinkedList<String>();
 
 			if (card % 2 == 0) {
 				for (int i = 0; i < card / 2; i++) {
-					queue1.offer(st.nextToken());
+					arr1.add(st.nextToken());
 				}
-				for (int i = card / 2; i < card; i++) {
-					queue2.offer(st.nextToken());
-
+				while (st.hasMoreTokens()) {
+					arr2.add(st.nextToken());
 				}
-
-			} else if (card % 2 == 1) {
+			} else {
 				for (int i = 0; i < card / 2 + 1; i++) {
-					queue1.offer(st.nextToken());
+					arr1.add(st.nextToken());
 				}
-				for (int i = card / 2 + 1; i < card; i++) {
-					queue2.offer(st.nextToken());
+				while (st.hasMoreTokens()) {
+					arr2.add(st.nextToken());
 				}
 			}
-
-			// 디버깅용 코드
-//			System.out.println(queue1.toString());
-//			System.out.println(queue2.toString());
-
 			StringBuilder sb = new StringBuilder();
-			while (!queue1.isEmpty() || !queue2.isEmpty()) {
-				if (!queue1.isEmpty()) {
-					sb.append(" ");
-					sb.append(queue1.poll());
-				}
-				if (!queue2.isEmpty()) {
-					sb.append(" ");
-					sb.append(queue2.poll());
-				}
+			sb.append("#").append(test);
+			while(!arr1.isEmpty()) {
+				sb.append(" ").append(arr1.poll());
+				if(!arr2.isEmpty())
+				sb.append(" ").append(arr2.poll());
 			}
-			System.out.println("#" + tc + sb);
+			System.out.println(sb);
 		}
 	}
 }
