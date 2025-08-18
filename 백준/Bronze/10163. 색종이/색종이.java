cd6292bfc7a99bcaa2c533f72379ccc176b645e0
최윤혁 <okccc5@naver.com>
2025-08-18 17:08:20 +0900

    [Bronze I] Title: 색종이, Time: 356 ms, Memory: 25320 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.java
+++ 백준/Bronze/10163. 색종이/색종이.java
@@ -18,7 +18,7 @@ public class Main {
 		// 처음 두개의 숫자 (x,y) 0,0이 왼쪽 아래임
 		// 다음 두개의 숫자는 너비(x길이) , 높이(y길이)임
 
-		int[][] arr = new int[101][101];
+		int[][] arr = new int[1001][1001];
 
 		for (int i = 1; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -26,12 +26,12 @@ public class Main {
 			int y1 = Integer.parseInt(st.nextToken());
 			
 			// 길이 너비
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
+			int width = Integer.parseInt(st.nextToken());
+			int	height = Integer.parseInt(st.nextToken());
 
 			// 겹치는건 덮어 씌우게 idx 삽입
-			for (int x = x1; x < x2+x1; x++) {
-				for (int y = y1; y < y2+y1; y++) {
+			for (int x = x1; x < width+x1; x++) {
+				for (int y = y1; y < height+y1; y++) {
 					arr[x][y] = i;
 				}
 			}
@@ -40,8 +40,8 @@ public class Main {
 		// 각 숫자 개수 세기
 		int[] ans = new int[N+1];
 		for (int A = 1; A <= N; A++) {
-			for (int i = 0; i < 101; i++) {
-				for (int j = 0; j < 101; j++) {
+			for (int i = 0; i < 1001; i++) {
+				for (int j = 0; j < 1001; j++) {
 					if(arr[i][j] == A)
 						ans[A]++;
 				}
