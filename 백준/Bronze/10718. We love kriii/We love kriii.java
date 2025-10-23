90a67adc8d5c0242c7a7be43a100f3c3d50a7af0
최윤혁 <okccc5@naver.com>
2025-10-24 08:51:02 +0900

    [Bronze V] Title: We love kriii, Time: 96 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st;
+//
+//		int cnt = 0;
+//		st = new StringTokenizer(br.readLine());
+//		for (int i = 0; i < 5; i++) {
+//			int a = Integer.parseInt(st.nextToken());
+//			cnt += a * a;
+//		}
+
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
