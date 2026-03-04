1381cf32e699e51ebd10d6d8c75dc6fd0bc1fec9
YunHyeok Choi <okccc5@naver.com>
2026-03-04 16:18:48 +0900

    [Bronze IV] Title: 시험 점수, Time: 68 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int B = Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken());
+        System.out.println(Math.max(A,B));
+
+        
+    }
+}
