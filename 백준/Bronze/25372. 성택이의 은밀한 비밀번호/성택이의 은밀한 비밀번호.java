d4013e26ae70d0ea6bbdf963edab843e9011a875
YunHyeok Choi <okccc5@naver.com>
2026-03-11 09:02:00 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 64 ms, Memory: 11848 KB -BaekjoonHub

--- 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -1,21 +1,27 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * 17472 다리 만들기 2
+ */
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
 
 		while (T-- > 0) {
 			String str = br.readLine();
 			int a = str.length();
-			if(a>=6 &&a<=9) {
-				System.out.println("yes");
-			}else {
-				System.out.println("no");
+			if (a >= 6 && a <= 9) {
+				sb.append("yes\n");
+			} else {
+				sb.append("no\n");
 			}
-			
+
 		}
+
+		System.out.print(sb);
 	}
 }
