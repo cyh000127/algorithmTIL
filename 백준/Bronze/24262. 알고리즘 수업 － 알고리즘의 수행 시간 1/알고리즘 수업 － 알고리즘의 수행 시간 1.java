03b22be6c019272e296f676fa719814bce973cd7
YunHyeok Choi <okccc5@naver.com>
2026-03-12 18:18:43 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 64 ms, Memory: 11372 KB -BaekjoonHub

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -1,7 +1,6 @@
 public class Main {
-	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("1\n0");
-		System.out.println(sb);
-	}
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
 }
