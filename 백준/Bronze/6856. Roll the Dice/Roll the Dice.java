93ee0a4f5ecbcdb2d4dcb76454e6661f37d155f6
YunHyeok Choi <okccc5@naver.com>
2026-03-27 17:11:25 +0900

    [Bronze III] Title: Roll the Dice, Time: 92 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/6856. Roll the Dice/Roll the Dice.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		int count = 0;
+		
+		for(int i = 1; i <= m; i++) {
+			for(int j = 1; j <= n; j++) {
+				if(i + j == 10) {
+					count++;
+				}
+			}
+		}
+		
+		if(count == 1) {
+			System.out.println("There is 1 way to get the sum 10.");
+		}else {
+			System.out.println("There are " + count + " ways to get the sum 10.");
+		}
+		sc.close();
+	}
+}
